3f3812f7b9926c4ad5a9d52d441893622c4903d3
iFuzzyC <34630228+iFuzzyC@users.noreply.github.com>
2017-12-28 15:23:21 -0200

    Update dna.py

--- MazeSolver/dna.py
+++ MazeSolver/dna.py
@@ -1,21 +1,7 @@
-# from miner import Miner
 class DNA():
     @staticmethod
     def calcFitness(path, idx, maze_grid, N):
         d = (path[idx].x + path[idx].y)/(maze_grid[-1].x + maze_grid[-1].y)
-        # if len(path)/(N**2) != 1:
-        #     d /= 5
+        if len(path)/(N**2) != 1:
+            d /= 5
         return d**2
-    @staticmethod
-    def crossover(father, mother):
-        bpath = []
-        if len(father.path) > len(mother.path):
-            n = len(mother.path)
-        else:
-            n = len(father.path)
-        i = 0
-        print(n)
-        while (father.path[i].index is mother.path[i].index) and i < n - 1:
-            bpath.append(father.path[i])
-            i += 1
-        return Miner(father.id_number, father.size, father.maze_grid, bpath)
